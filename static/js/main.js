4bf7188665ac00035c5ba0b684938304e9179e1d
Scott Ammon <scott.o.ammon@gmail.com>
2018-08-03 19:19:02 -0700

    updating home page

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,3 @@
 $(document).ready(function(){
   console.log('ready!');
-  M.AutoInit();
-
-  function setup() {
-    var myCanvas = createCanvas(600, 400);
-    myCanvas.parent('myCanvas');
-    line(15, 25, 70, 90);
-  }
 });
