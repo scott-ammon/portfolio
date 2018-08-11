06bafa111f2189c3bb9c60a0a3510f16d58f2557
Scott Ammon <scott.o.ammon@gmail.com>
2018-08-11 10:39:23 -0700

    working on project page style

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   console.log('ready!');
 
-  $( ".proj-text" ).mouseenter(function() {
-    $(".proj-text p").css("display", "inline-block");
-  }).mouseleave(function() {
-    $(".proj-text p").css("display", "none");
-  });
+  // $( ".proj-text" ).mouseenter(function() {
+  //   $(".proj-text p").css("display", "block");
+  // }).mouseleave(function() {
+  //   $(".proj-text p").css("display", "none");
+  // });
 });
