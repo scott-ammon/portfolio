da36db7435325e454fb948c29787dc5fd7978e4b
Scott Ammon <scott.o.ammon@gmail.com>
2018-08-12 09:07:33 -0700

    updating nav links and about page

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   console.log('ready!');
 
-  // $( ".proj-text" ).mouseenter(function() {
-  //   $(".proj-text p").css("display", "block");
-  // }).mouseleave(function() {
-  //   $(".proj-text p").css("display", "none");
-  // });
+  $('.hamburger').click(function() {
+    $(this).toggleClass('is-active');
+    $("#nav-menu").slideToggle("slow");
+  });
+
 });
