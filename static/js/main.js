36944075a5813465625c5fc0cace07340c0df5a8
Scott Ammon <scott.o.ammon@gmail.com>
2018-08-12 15:32:54 -0700

    fixing styles on home page

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   console.log('ready!');
 
-  $('.hamburger').click(function() {
+  $('.navbar').click(function() {
     $(this).toggleClass('is-active');
     $("#nav-menu").slideToggle("slow");
     // $("#nav-menu").toggle("slide");
