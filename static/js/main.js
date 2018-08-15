b2358d2e57852f736dbf92aa33c828c12af59b7d
Scott Ammon <scott.o.ammon@gmail.com>
2018-08-15 14:59:16 -0700

    updating project page styles, cleaning up unused variables

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-  console.log('ready!');
-
-  $('.navbar').click(function() {
+  $('.nav-button').click(function() {
+    // Add class to fill in menu circle
     $(this).toggleClass('is-active');
+    // Display menu div by sliding down
     $("#nav-menu").slideToggle("slow");
-    // $("#nav-menu").toggle("slide");
+    // Scroll to top of page if menu clicked
     $("html, body").animate({ scrollTop: 0 }, "slow");
   });
-
 });
